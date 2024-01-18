2dc38738aeb5e9ec4dce90b16ff51875884a83e2
bl3rt4 <blerta.aazemi@gmail.com>
2024-01-17 22:11:40 -0800

    SS-40 update

--- client/SudokuSolver.php
+++ client/SudokuSolver.php
@@ -76,7 +76,7 @@ document.getElementById("sudoku-board").addEventListener("input", function (even
 
                 cell.textContent = '';
                 cell.style.color='';
-                ceel.style.border='';
+                cell.style.border='';
             }
         }
 
